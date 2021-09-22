7013ccf7880a4ffc9013a50292f333b0b6231a7f
panke <panke@gbps.com>
2021-09-22 16:06:13 +0800

    新增地址

--- app01/pages/addAddress/addAddress.vue
+++ app01/pages/addAddress/addAddress.vue
@@ -1,6 +1,9 @@
 <template>
-	<view>
-		<text>111</text>
+	<view class="container">
+		<view class="input-content">
+			<view class="title">收货人</view>
+			<input class="uni-input" focus placeholder="姓名" />
+		</view>
 	</view>
 </template>
 
@@ -17,6 +20,23 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+	.container{
+		.input-content{
+			display: flex;
+			padding: 10px 10px;
+			border-bottom: 1px solid #e7e7e7;
+			.title{
+				width: 85px;
+				color: #999999;
+				font-size: 14px;
+			}
+			.uni-input{
+				
+				font-size: 14px;
+				color: #e7e7e7;
+			}
+		}
+	}
+	
 </style>
